f4dba0b658f6702ec3b6227f6555e23078cc3c07
eshal annisa niagara <eshal.niagara@gmail.com>
2025-11-29 13:43:24 +0700

    Create PoliFactory.php

---
+++ database/factories/PoliFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Poli>
+ */
+class PoliFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama'  => $this->faker->name(),
+            'biaya' => $this->faker->numberBetween(50000, 300000),
+        ];
+    }
+}
